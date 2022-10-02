6dd8b4d2461aab2d6ac9ea35310860f18f0d257d
RealHenryMartinez <henrymtz0390@gmail.com>
2022-10-02 13:20:24 -0700

    frontend

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Game = () => {
   return (
     <div className='flex max-w-none bg-[#3d2c3c] py-10 justify-center'>
-      <iframe src="https://python.HenryMartinez8.repl.co/" width="1000" height="1000" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe> 
+      <iframe src="https://pythongame.HenryMartinez8.repl.co/" width="1000" height="1000" frameborder="no" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" scrolling="no"></iframe> 
     </div>
   )
 }
